19f1877ebcd6e0a36b959c5df91257383fb3347c
Javier Rojas <53715023+jerojash@users.noreply.github.com>
2020-07-20 21:05:00 -0400

    added function agregarPersona();

--- proyecto.cpp
+++ proyecto.cpp
@@ -12,13 +12,13 @@ struct lugar{
 	char direccion[40];
 };
 
-struct infracciones{
+struct infraccion{
 	int numeroInfraccion;
 	struct fecha fechaInfraccion;
 	char tipoInfraccion[40];
 	int monto;
 	int pagado;
-	struct infracciones *prox;
+	struct infraccion *infraccionProx;
 };
 
 struct vehiculos{
@@ -27,7 +27,7 @@ struct vehiculos{
 	char modelo[10];
 	struct fecha annio;
 	char color[10];
-	struct vehiculos *prox;
+	struct vehiculos *vehiculoProx;
 	struct infracciones * datosInfraccion;
 };
 
@@ -39,7 +39,13 @@ struct persona{
 	struct persona *personaProx;
 };
 
+struct persona *p = NULL;
+struct vehiculo *v = NULL;
+struct infraccion *i =NULL;
+
 void encabezado();
+
+void agregarPersona(struct persona **p);
 
 void menuPersonas();
 
@@ -51,11 +57,27 @@ void menuConsultas();
 
 void menuOperacionesConsultas();
 
-void menuPrincipal();
 
-int main(){
-	
-	menuPrincipal();
+int main(){         //*************************FUNCION PRINCIPAL***************************
+
+
+	int opcion = 1;	
+	while(opcion){ //   **************************MENU PRINCIPAL*************************
+		encabezado();
+		printf("\t\t\t\t {POR FAVOR ESCRIBA LA OPCION QUE DESEA}\n\n");
+		printf("\t\t\t\t\t(1)--MANTENIMIENTO\n");
+		printf("\t\t\t\t\t(2)--OPERACIONES Y CONSULTAS\n\n");
+		printf("\t\t\t\t\t(0)--SALIR\n\n\t\t\t\t\t\t\t");
+		scanf("%i",&opcion);
+		system("cls");
+		
+		switch (opcion){
+			case 1: menuMantenimiento();//LLAMADA A LA FUNCION menuMantenimiento
+				break;
+			case 2: menuOperacionesConsultas();//LLAMADA A LA FUNCION menuOperaciones
+				break;
+		}
+	}
 	return 0;
 }
 
@@ -63,6 +85,53 @@ int main(){
 void encabezado(){
 		printf("\t\t\t\t\tMENU DE CONTROL DE MULTAS\n");
 		printf("\t\t\t\tALCALDIA DEL MUNICIPIO DE CHACAO, CARACAS\n\n");
+}
+
+void agregarPersona(struct persona **p){
+	int num;
+	char c;
+	while ((c = getchar()) != '\n' && c != EOF);
+	struct persona *aux = new struct persona;
+	system("cls");
+	printf("\n\t\t\tIngrese el nombre: "); 
+	gets(aux->nombre);
+	printf("\n\t\t\tIngrese la cedula: ");
+	scanf("%li",&aux->cedula);
+	printf("\n\t\t\tIngrese el dia (dd) de nacimiento: ");
+	scanf("%i",&aux->fechaNacimiento.dd);
+	while((aux->fechaNacimiento.dd>31||(!aux->fechaNacimiento.dd))||(aux->fechaNacimiento.dd<0)){
+		system("cls");
+		printf("\n\n\t\t\t\tIngrese un valor valido (1-31)\n\n");
+		system("pause");
+		system("cls");
+		printf("\n\n\t\t\tIngrese el nombre: %s",aux->nombre);
+		printf("\n\n\t\t\tIngrese la cedula: %li",aux->cedula);	
+		printf("\n\n\t\t\tIngrese el dia (dd) de nacimiento: ");
+		scanf("%i",&(*aux).fechaNacimiento.dd);
+	}
+	printf("\n\t\t\tIngrese el mes (mm) de nacimiento: ");
+	scanf("%i",&aux->fechaNacimiento.mm);
+	while((aux->fechaNacimiento.mm>12)||(aux->fechaNacimiento.mm==0)||(aux->fechaNacimiento.mm<0)){
+		system("cls");
+		printf("\n\n\t\t\t\tIngrese un valor valido (1-12)\n\n");
+		system("pause");
+		system("cls");
+		printf("\n\n\t\t\tIngrese el nombre: %s",aux->nombre);
+		printf("\n\n\t\t\tIngrese la cedula: %li",aux->cedula);	
+		printf("\n\n\t\t\tIngrese el dia (dd) de nacimiento: %i",aux->fechaNacimiento.dd);
+		printf("\n\n\t\t\tIngrese el mes (mm) de nacimiento: ");
+		scanf("%i",&aux->fechaNacimiento.mm);
+	}
+	printf("\n\t\t\tIngrese el a%co (yyyy) de nacimiento: ",164);
+	scanf("%i",&aux->fechaNacimiento.yy);
+	printf("\n\n\t\t\t     Desea guardar estos datos?");
+	printf("\n\t\t      Escriba (1) si desea cargar los datos: ");
+	scanf("%i",&num);
+	if (num == 1){
+		aux->personaProx= *p;
+		*p = aux;
+	}
+	system("cls");
 }
 
 void menuPersonas(){
@@ -81,17 +150,17 @@ int opcion;
 		system("cls");
 		
 		switch (opcion){
-			case 1: //LLAMADA A LA FUNCION agregarPersona
+			case 1: agregarPersona(&p);//LLAMADA A LA FUNCION agregarPersona
 				break;
 			case 2: //LLAMADA A LA FUNCION modificarPersona
 				break;
 			case 3: //LLAMADA A LA FUNCION consultarPersona
 				break;
 			case 4: //LLAMADA A LA FUNCION borrarPersona
+
 				break;
 		}
 	}
-	
 }
 
 void menuVehiculos(){
@@ -224,26 +293,3 @@ void menuOperacionesConsultas(){
 	}
 }
 
-void menuPrincipal(){
-	
-	
-	int opcion = 1;	
-	while(opcion){
-		encabezado();
-		printf("\t\t\t\t {POR FAVOR ESCRIBA LA OPCION QUE DESEA}\n\n");
-		printf("\t\t\t\t\t(1)--MANTENIMIENTO\n");
-		printf("\t\t\t\t\t(2)--OPERACIONES Y CONSULTAS\n\n");
-		printf("\t\t\t\t\t(0)--SALIR\n\n\t\t\t\t\t\t\t");
-		scanf("%i",&opcion);
-		system("cls");
-		
-		switch (opcion){
-			case 1: menuMantenimiento();//LLAMADA A LA FUNCION menuMantenimiento
-				break;
-			case 2: menuOperacionesConsultas();//LLAMADA A LA FUNCION menuOperaciones
-				break;
-		}
-	}
-	
-
-}
